67f100cf5c42a9069d025a74f6fac83b9c108c43
Yasith Vidanaarachchi <yasith.vidanaarachchi@gmail.com>
2010-10-10 15:03:41 +0530

    Re-implemented the gridder, now with more/less squares buttons working

--- src/amazingartist/AmazingArtist.java
+++ src/amazingartist/AmazingArtist.java
@@ -12,7 +12,7 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 
 package amazingartist;
 
@@ -25,8 +25,12 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -39,50 +43,110 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
-class LoadedImage extends Component {
+class JImagePanel extends JPanel {
+
+	private BufferedImage img;
+	int x, y;
 	
-	BufferedImage image;
-	
-	public void paint(Graphics g) {
-		g.drawImage(image, 0, 0, null);
+	int trans = 80;
+	int maskx = 0;
+	int masky = 0;
+	int parts = 5;
+
+	public JImagePanel(BufferedImage image, int x, int y) {
+		super();
+		this.img = image;
+		this.x = x;
+		this.y = y;
 	}
-	
-	public LoadedImage(String file) {
-		try{
-			image = ImageIO.read(new File(file));
-		}catch (IOException e){
-			// TODO 
+
+	protected void drawGrid(Graphics g) {
+		super.paintComponents(g);
+		Graphics2D g2 = (Graphics2D) g;
+		g2.drawImage(img, 0, 0, null);
+
+		int w = img.getWidth();
+		int h = img.getHeight();
+
+		double size = w / parts;
+
+		g2.setColor(Color.BLACK);
+
+		// Drawing vertical lines
+		
+		for(int i = 0; size*i < w; i++){
+			g2.draw(new Line2D.Double(size * i, 0.00, size * i, (double) h));	
 		}
+
+		// Drawing horizontal lines
+		for(int i = 0; size * i < h; i++){
+			g2.draw(new Line2D.Double(0.00, size * i, (double) w, size * i));	
+		}
+		
+		// Drawing the mask
+		for(int i = 0; size * i < w; i++){
+			for(int j = 0; size * j < h; j++){
+				double sx = size * i;
+				double sy = size * j;
+				double ex = Math.min((double)w, size*(i+1));
+				double ey = Math.min((double)h, size*(j+1));
+				
+				Rectangle2D rect = new Rectangle2D.Double(sx, sy, size, size);
+				
+			}
+		}
+		
 	}
-	
-	public Dimension getPreferredSize() {
-		if(image == null) {
-			return new Dimension(100, 100);
-		} else {
-			return new Dimension(image.getWidth(), image.getHeight());
-		}
+
+	/**
+	 * Sets the number of parts the image should be split into
+	 * 
+	 * @param val
+	 *            +1 or -1 to increase or to decrease the number of parts
+	 */
+	protected void setParts(int val) {
+		this.parts += val;
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponents(g);
+		this.drawGrid(g);
 	}
 }
 
-public class AmazingArtist{
-	
-	Canvas canvas = new Canvas();
-	BufferedImage img;
-	String file = "images/test.jpg";
-	private BufferStrategy strategy;
-	
-	
-	public AmazingArtist(){
-		JFrame container = new JFrame("AmazingArtist");
-		
+public class AmazingArtist implements ActionListener {
+
+	JImagePanel panel;
+
+	JButton load = new JButton("Load image");
+	JButton incr = new JButton("More squares");
+	JButton decr = new JButton("Lesser squares");
+	JButton tran = new JButton("Transparent");
+	JButton opaq = new JButton("Opaque");
+
+
+	public void displayImage(JFrame frame) {
+		BufferedImage img = null;
+		try {
+			img = ImageIO.read(new File("images/test.jpg"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		panel = new JImagePanel(img, 0, 0);
+
+		frame.add(panel, BorderLayout.CENTER);
+		frame.setBounds(0, 0, img.getWidth(), img.getHeight());
+		frame.setVisible(true);
+
+	}
+
+	public void addButtons(JFrame container) {
+
 		container.setLayout(new BorderLayout());
-		
-		JButton load = new JButton("Load image");
-		JButton incr = new JButton("More squares");
-		JButton decr = new JButton("Lesser squares");
-		JButton tran = new JButton("Transparent");
-		JButton opaq = new JButton("Opaque");
-		
+
 		JPanel toolbar = new JPanel();
 		toolbar.setLayout(new GridLayout(1, 5));
 		toolbar.add(load);
@@ -90,62 +154,49 @@ public class AmazingArtist{
 		toolbar.add(decr);
 		toolbar.add(tran);
 		toolbar.add(opaq);
+
+		load.addActionListener(this);
+		incr.addActionListener(this);
+		decr.addActionListener(this);
+		tran.addActionListener(this);
+		opaq.addActionListener(this);
 		
 		container.add(toolbar, BorderLayout.NORTH);
-		
-		//TODO decent loading of the image
-		
-		try {
-			img = ImageIO.read(new File(file));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		Dimension PrefDim = new Dimension(img.getWidth(), img.getHeight());
-		
-		System.out.println(PrefDim.width + "x" + PrefDim.height);
-		
-		JPanel panel = new JPanel();
-		panel.setPreferredSize(PrefDim);
-		panel.setLayout(null);
-		//panel.add(canvas);
-		
-		canvas.setPreferredSize(PrefDim);
-		canvas.setIgnoreRepaint(true);
-		
-		container.add(canvas, BorderLayout.CENTER);
-		container.pack();
-		container.setResizable(false);
-		container.setVisible(true);
-		
-		container.addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent e){
-				System.exit(0);
-			}
-		});
-		
-		
-		// TODO add a key listener
-		
-		canvas.requestFocus();
-		
-		canvas.createBufferStrategy(2);
-		strategy = canvas.getBufferStrategy();
-		
-		// Display the default image
-		Graphics2D g = (Graphics2D) strategy.getDrawGraphics();
-		g.drawImage(img, 0, 0, null);
-		g.dispose();
-		strategy.show();
+
 	}
-	
+
+	void drawGrid(Graphics g) {
+		panel.drawGrid(g);
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		AmazingArtist artist = new AmazingArtist();
+		JFrame container = new JFrame("AmazingArtist");
+		
+		container.addWindowListener(new WindowAdapter() {
+			@SuppressWarnings("unused")
+			public void WindowClosing(WindowEvent e){
+				System.exit(0);
+			}
+		});
+		
+		artist.addButtons(container);
+		artist.displayImage(container);
+		artist.drawGrid(container.getRootPane().getGraphics());
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource().equals(incr)){
+			panel.setParts(1);
+			panel.repaint();
+		} else if(e.getSource().equals(decr)){
+			panel.setParts(-1);
+			panel.repaint();
+		}
 	}
 
 }
